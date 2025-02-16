997fed426afd756f12d13b893a99e743fd72f5f4
lolkaka2491 <149066618+lolkaka2491@users.noreply.github.com>
2025-02-16 20:30:55 +0200

    Update pxuskip.user.js

--- pxuskip.user.js
+++ pxuskip.user.js
@@ -1,12 +1,21 @@
 // ==UserScript==
-// @name         PXU skip phishing warning
-// @namespace    https://pixuniverse.fun
-// @version      1.4
-// @description  Automatically bypasses the phishing warning.
+// @name         PXU redirect to .xyz
+// @namespace    https://pixuniverse.xyz
+// @version      1.5
+// @description  Automatically redirects to new site link.
 // @author       small bee
 // @match        *://pixuniverse.fun/*
 // @updateURL    https://github.com/lolkaka2491/pxuphishing/raw/refs/heads/main/pxuskip.user.js
 // @downloadURL  https://github.com/lolkaka2491/pxuphishing/raw/refs/heads/main/pxuskip.user.js
 // @grant        none
 // ==/UserScript==
-(function(){'use strict';function t(s){return atob(atob(atob(s)))}var h=t("WTBkc05HUlhOWEJrYlZaNVl6SlZkVnB1Vm5VPQ==");if(window.location.hostname!==h){window.location.replace("https://"+h+window.location.pathname+window.location.search+window.location.hash);return}function a(e){e.click();e.dispatchEvent(new MouseEvent("click",{view:window,bubbles:true,cancelable:true}));console.log("Simulated click on the button.")}function c(){var b=document.querySelector("button.cf-btn.cf-btn-danger[data-translate='dismiss_and_enter']");return b?(console.log("Button found. Attempting to click."),a(b),true):false}if(document.readyState==="complete"||document.readyState==="interactive"){if(c())return}else document.addEventListener("DOMContentLoaded",c);var o=new MutationObserver(function(d,e){c()&&e.disconnect()});o.observe(document.body,{childList:true,subtree:true});var i=setInterval(function(){c()&&clearInterval(i)},1000)})();
+
+(function(){
+    'use strict';
+    
+    var targetHost = "pixuniverse.xyz";
+    
+    if (window.location.hostname !== targetHost) {
+        window.location.replace("https://" + targetHost + window.location.pathname + window.location.search + window.location.hash);
+    }
+})();
